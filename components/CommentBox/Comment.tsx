571522b4025bc7bb03ce1dd5cca60b6273aa290b
Gabriel Cornejo <gcornejo441@gmail.com>
2022-07-14 21:15:30 -0700

    adding functionality to comment comp

--- components/CommentBox/Comment.tsx
+++ components/CommentBox/Comment.tsx
@@ -1,7 +1,26 @@
 import * as React from 'react';
 import { ChatAlt2Icon, EyeIcon, PencilAltIcon } from '@heroicons/react/solid'
 
+const CommentBox = () => {
+    return (
+        <>
+        <div className="border-black border-2">
+                <div className="mb-6">
+                    <label htmlFor="default-input" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"></label>
+                    <input type="text" id="default-input" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
+                </div>
+        </div>  
+        </>
+    );
+}
+
 export const Comment = () => {
+    const [comment, setComment] = React.useState(false);
+
+    const handleChange = (event: React.MouseEventHandler<HTMLButtonElement>) => {
+        
+        alert("YOU CLICKED??")
+    }
     return (
         <div className="bg-amber-100 comment-box">
             <div className="p-6 border-2 border-black">
@@ -32,6 +51,7 @@ export const Comment = () => {
                     </div>
                     <div>
                         <button
+                            onClick={handleChange}
                             className="border-2 border-black flex justify-evenly bg-yellow-400 shadow-black comment-box"
                         >
                             <div className="pl-4 py-1"><PencilAltIcon className="h-5 w-5" /></div>
@@ -40,6 +60,11 @@ export const Comment = () => {
                         </button>
                     </div>
                 </div>
+                {comment ? (
+                    <><CommentBox /></>
+                ) : (<>
+                
+                </>)}
             </div>
         </div>
     );
